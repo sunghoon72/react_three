e6550ea8067d3af4e24535dfeb01c46fd644b3b1
sunghoon72 <71563014+sunghoon72@users.noreply.github.com>
2022-11-21 22:49:58 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Suspense } from "react";
+import "./styles.css";
+import styled from "styled-components";
+
+import Background from "./components/Background";
+import TextSection from "./components/TextSection";
+import Box from "./components/Box";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Sphere from "./components/AnimatedSphere";
+
+export default function App() {
+  return (
+    <Wrapper className="App">
+      <Background />
+      <TextSection />
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
+      </Canvas>
+      <Canvas className="canvas">
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Sphere />
+        </Suspense>
+      </Canvas>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+  background: #1f1144;
+
+  canvas {
+    height: 500px;
+  }
+`;
